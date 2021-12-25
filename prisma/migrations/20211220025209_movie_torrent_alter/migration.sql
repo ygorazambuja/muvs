0ffd48497111fb1f4e25b687b97db0fa3a26b563
Ygor Azambuja <ygorazambuja@gmail.com>
2021-12-25 01:49:41 -0400

    chore: remove migrations

--- prisma/migrations/20211220025209_movie_torrent_alter/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Torrent" (
-    "uuid" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "link" TEXT NOT NULL,
-    "movieUuid" TEXT,
-    CONSTRAINT "Torrent_movieUuid_fkey" FOREIGN KEY ("movieUuid") REFERENCES "Movie" ("uuid") ON DELETE SET NULL ON UPDATE CASCADE
-);
